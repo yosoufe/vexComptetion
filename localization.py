3ea35b01cb26d71cb1982522b22d9f55ba4bd0f3
Yousof <yousofebneddin@yahoo.com>
2023-09-19 19:58:30 -0700

    small update

--- localization.py
+++ localization.py
@@ -21,7 +21,7 @@ class ATLocalizerNode(Node):
                                quad_sigma=0.0,
                                refine_edges=1,
                               #  decode_sharpening=0.25,
-                               decode_sharpening=0.4,
+                               decode_sharpening=0.3,
                                debug=0)
     tags = self.detector.detect(
       img = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY),
